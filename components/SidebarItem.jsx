818dd8cb45d4314ace3d309341bbc98d0ea6fde9
harkishan1993 <113414021+harkishan1993@users.noreply.github.com>
2023-06-18 03:41:54 -0700

    logoupdate

---
+++ components/SidebarItem.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { MdArrowDropUp, MdArrowDropDown } from 'react-icons/md'
+import MyLink from "./Mylink"
+export default function SidebarItem({ item }) {
+    const [open, setOpen] = useState(false)
+    if (item.childrens) {
+        return (
+            <div className={open ? "sidebar-item open" : "sidebar-item"}>
+                <div className="sidebar-title">
+                    <MyLink href={item.path || "#"} className="navlink">
+                        {item.title}
+                    </MyLink>
+                    <span><MdArrowDropUp className='text-[40px] toggle-btn text-white' onClick={() => setOpen(!open)} /></span>
+                </div>
+                <div className="sidebar-content navlink flex flex-col gap-2 pl-2">
+                    {item.childrens.map((child, index) => <SidebarItem key={index} item={child} />)}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+                <MyLink href={item.path || "#"} className="navlink">
+                    {item.title}
+                </MyLink>
+        )
+    }
+}
---
+++ data/sidebar.js
@@ -0,0 +1,92 @@
+export const items = [
+    {
+        "title": "About Us",
+        "path": "/about-us"
+    },
+    {
+        "title": "Products",
+        "path": "/products",
+        "childrens": [
+            {
+                "title": "Digital Indicators",
+                "path": "/",
+            },
+            {
+                "title": "Temperature Controllers",
+                "path": "/"
+            },
+            {
+                "title": "Data Loggers & Scanners",
+                "path": "/"
+            },
+            {
+                "title": "Temperataure & Humidity Instruments",
+                "path": "/"
+            },
+            {
+                "title": "Thyristror Triggering Cards",
+                "path": "/"
+            },
+            {
+                "title": "Thyristore Power Controller",
+                "path": "/"
+            },
+            {
+                "title": "Product Accessories",
+                "path": "/"
+            },
+        ]
+    },
+    {
+        "title": "Services",
+        "path": "/services",
+        "childrens": [
+            {
+                "title": "Data Acquisition Software",
+                "path": "/"
+            },
+            {
+                "title": "Internet of Things",
+                "path": "/"
+            },
+            {
+                "title": "Post Weld Heat Treatment",
+                "path": "/"
+            },
+        ]
+    },
+    {
+        "title": "Industries",
+        "paht":"/industries",
+        "childrens": [
+            {
+                "title": "Glass Manufactures",
+                "path": "/"
+            },
+            {
+                "title": "Jewellery Manufactures",
+                "path": "/"
+            },
+            {
+                "title": "Pharmaceuticals",
+                "path": "/"
+            },
+            {
+                "title": "Steel Plants",
+                "path": "/"
+            }
+        ]
+    },
+    {
+        "title": "Applications",
+        "path": "/"
+    },
+    {
+        "title": "Contact Us",
+        "path": "/contact-us"
+    },
+    {
+        "title": "Login",
+        "path": "/login"
+    }
+]

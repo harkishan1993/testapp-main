43c0742b5af6e7f8cf8062c2437c2fbc6012a1f8
harkishan1993 <113414021+harkishan1993@users.noreply.github.com>
2023-06-17 20:17:21 -0700

    csstextupdate

--- components/Homesectionone.jsx
+++ components/Homesectionone.jsx
@@ -8,7 +8,7 @@ function Homesectionone() {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             <div className='flex flex-col items-center justify-center gap-4 order-2 md:order-1'>
                 <h1 className='headtext'>Partners in Measurement $ Control</h1>
-                <p className='textbase'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima error adipisci consequatur consectetur delectus dolore, nam, nihil quo architecto vero voluptatem, quia dolor dignissimos laborum perferendis! Modi fugit repudiandae ratione.</p>
+                <p className='textbase textset'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima error adipisci consequatur consectetur delectus dolore, nam, nihil quo architecto vero voluptatem, quia dolor dignissimos laborum perferendis! Modi fugit repudiandae ratione.</p>
                 <MyLink href='/products' className='capitalize btn'>
                     our product
                 </MyLink>

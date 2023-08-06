0d202e187a27c93cfc4455c4738099198d637f69
harkishan patel <harkishanmotka@gmail.com>
2023-08-06 12:24:38 -0700

    ASDSADASSDAda

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -1,20 +1,20 @@
 
-// import Pageheader from "@/components/Pageheader"
-// import Sectionone from "./components/Sectionone"
-// import {Productdetails} from "@/data/productpage"
-// export async function generateStaticParams() {
-//     return Productdetails.map((product) => ({
-//       slug: product.category,
-//     }))
-//   }
-// function page({ params }) {
-//     const {slug} = params
-//   return (
-//     <>
-//      <Pageheader pageTitle='Products' />
-//      <Sectionone productSlug = {slug} />
-//     </>
-//   )
-// }
+import Pageheader from "@/components/Pageheader"
+import Sectionone from "./components/Sectionone"
+import {Productdetails} from "@/data/productpage"
+export async function generateStaticParams() {
+    return Productdetails.map((product) => ({
+      slug: product.category,
+    }))
+  }
+function page({ params }) {
+    const {slug} = params
+  return (
+    <>
+     <Pageheader pageTitle='Products' />
+     <Sectionone productSlug = {slug} />
+    </>
+  )
+}
 
-// export default page
+export default page

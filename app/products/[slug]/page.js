6c1b9a709fd609b64d21f5221caed2c9e0e1067f
harkishan patel <harkishanmotka@gmail.com>
2023-08-06 12:29:43 -0700

    AWDSADA

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -1,18 +1,18 @@
 
 import Pageheader from "@/components/Pageheader"
-import Sectionone from "./components/Sectionone"
-import {Productdetails} from "@/data/productpage"
-export async function generateStaticParams() {
-    return Productdetails.map((product) => ({
-      slug: product.category,
-    }))
-  }
+// import Sectionone from "./components/Sectionone"
+// import {Productdetails} from "@/data/productpage"
+// export async function generateStaticParams() {
+//     return Productdetails.map((product) => ({
+//       slug: product.category,
+//     }))
+//   }
 function page({ params }) {
-    const {slug} = params
+    // const {slug} = params
   return (
     <>
      <Pageheader pageTitle='Products' />
-     <Sectionone productSlug = {slug} />
+     {/* <Sectionone productSlug = {slug} /> */}
     </>
   )
 }
--- app/products/page.js
+++ app/products/page.js
@@ -1,14 +1,14 @@
 import Pageheader from "@/components/Pageheader"
 import Sectionone from "./components/Sectionone"
 async function page() {
-  let product = await fetch(`https://tenaciousinstrument.in/api/product`,{
-      cache:'no-cache'
-     }).then((r)=>r.json()) 
+  // let product = await fetch(`https://tenaciousinstrument.in/api/product`,{
+  //     cache:'no-cache'
+  //    }).then((r)=>r.json()) 
  
   return (
     <>
        <Pageheader pageTitle='Products' />
-       <Sectionone product = {product} />
+       {/* <Sectionone product = {product} /> */}
     </>
   )
 }

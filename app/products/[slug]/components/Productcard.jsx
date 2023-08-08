8c51fc7eeea905fe9a3f7e4b0c50a9df7cdf0947
harkishan patel <harkishanmotka@gmail.com>
2023-08-08 12:19:41 -0700

    addfewnewmovement

--- app/products/[slug]/components/Productcard.jsx
+++ app/products/[slug]/components/Productcard.jsx
@@ -2,19 +2,22 @@
 import Myimage from '@/components/Myimage'
 import MyLink from '@/components/Mylink'
 import {FaRegHeart} from 'react-icons/fa'
-function Productcard({ name, price, path = "/" }) {
+function Productcard({ name, path, image }) {
+    
+    let image1 = !!image?.[0]?.url ? JSON.parse(image?.[0]?.url) : []
+    let url = image1?.[0]?.secure_url || '/'
     return (
         <div className="w-[100%] p-1 flex flex-col justify-between items-center rounded-[10px] shadowapply">
-            <Myimage src='/img/THW-1.jpg' width={130} height={130} className='w-[100%] h-auto rounded-[10px]' priority={true} />
+            <Myimage src={url} width={130} height={130} className='w-[100%] h-auto rounded-[10px]' priority={true} />
             <div className="p-4 flex flex-col justify-center text-center gap-2">
-                <h3 className="productname"><MyLink href={path}>{name}</MyLink></h3>
+                <h3 className="productname"><MyLink href={`/products/${path}`}>{name}</MyLink></h3>
                 <span className="productprice">{price}</span>
-                <MyLink href='/products/temperature-controllers/portable-thermometer-dtm-21' className="cartbtn">
+                <MyLink href={`/products/${path}`} className="cartbtn">
                         view details
                 </MyLink>
                 <div className="flex items-center justify-center gap-2 text-emerald-800">
                     <FaRegHeart/>
-                    <span>Add to Wishlist</span>
+                    {/* <span>Add to Wishlist</span> */}
                 </div>
             </div>
         </div>

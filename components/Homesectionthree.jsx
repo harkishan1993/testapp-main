cebfe931306d4c3edc1213a2d20beb9753e41314
harkishan patel <harkishanmotka@gmail.com>
2023-08-12 01:53:12 -0700

    dsadasdfas

--- components/Homesectionthree.jsx
+++ components/Homesectionthree.jsx
@@ -10,18 +10,18 @@ function Homesectionthree() {
                 <Sectiontitle>Our Applications</Sectiontitle>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[100%] gap-[20px] md:gap-[40px]">
                         <div className="p-1 flex flex-col items-center justify-between rounded-[10px] shadowapply">
-                            <Myimage src='/img/application.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
-                            <h3 className="productname mx-4 my-2"><MyLink href='/'>SCR Power Regulators</MyLink></h3>
+                            <Myimage src='/img/1_Building Automation.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
+                            <h3 className="productname mx-4 my-2"><MyLink href='/'>Building Automation</MyLink></h3>
                             {/* <MyLink href='/' className="linktext mx-4 my-2">read more</MyLink> */}
                         </div>
                         <div className="p-1 flex flex-col items-center rounded-[10px] shadowapply">
-                            <Myimage src='/img/application.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
-                            <h3 className="productname mx-4 my-2"><MyLink href='/'>SCR Triggering Cards for OEM</MyLink></h3>
+                            <Myimage src='/img/2_Server Room.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
+                            <h3 className="productname mx-4 my-2"><MyLink href='/'>Server Room</MyLink></h3>
                             {/* <MyLink href='/' className="linktext mx-4 my-2">read more</MyLink> */}
                         </div>
                         <div className="p-1 flex flex-col items-center rounded-[10px] shadowapply">
-                            <Myimage src='/img/application.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
-                            <h3 className="productname mx-4 my-2"><MyLink href='/'>Thyristor Power Switches</MyLink></h3>
+                            <Myimage src='/img/3_Pharma Lab.jpg' width={150} height={80} className='w-[100%] h-auto rounded-[10px]' priority={false} />
+                            <h3 className="productname mx-4 my-2"><MyLink href='/'>Pharma Lab</MyLink></h3>
                             {/* <MyLink href='/' className="linktext mx-4 my-2">read more</MyLink> */}
                         </div>
                 </div>
